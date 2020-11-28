3ee27c24e05828c437a7b3091a3554301644a66d
emtupp <emilyc.tupper@gmail.com>
2020-11-28 16:41:47 +0000

    populate items table

--- db/seeds/02_list.sql
+++ db/seeds/02_list.sql
@@ -1,10 +1,1 @@
-INSERT INTO list (email, password, username)
-VALUES ('kingdom@hearts.com', 'password', 'sora');
-INSERT INTO list (email, password, username)
-VALUES ('cloud@ff7.com', 'password', 'cloud');
-INSERT INTO list (email, password, username)
-VALUES ('ellie@tlou2.com', 'password', 'ellie');
-INSERT INTO list (email, password, username)
-VALUES ('Geralt@blackiven.com', 'password', 'Geralt');
-INSERT INTO list (email, password, username)
-VALUES ('Triss@merigold.com', 'password', 'Triss');
+
---
+++ db/seeds/03_categories.sql
@@ -0,0 +1,10 @@
+INSERT INTO categories (category)
+VALUES ('movies');
+INSERT INTO categories (category)
+VALUES ('restaurants');
+INSERT INTO categories (category)
+VALUES ('products');
+INSERT INTO categories (category)
+VALUES ('books');
+INSERT INTO categories (category)
+VALUES ('miscellaneous');
