c6b597324ac77e5962f8cf12e48096efdf215d32
emtupp <emilyc.tupper@gmail.com>
2020-11-28 15:54:32 +0000

    populate users table with fake users for db

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,31 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (email, password, username)
+VALUES ('tedmosbyarchitect@himym.com', 'password', 'theodore');
+INSERT INTO users (email, password, username)
+VALUES ('legen_dary@himym.com', 'password', 'barnacle');
+INSERT INTO users (email, password, username)
+VALUES ('judgefudge@himym.com', 'password', 'marshmellow');
+INSERT INTO users (email, password, username)
+VALUES ('robinmetronews1@himym.com', 'password', 'hoser') ;
+INSERT INTO users (email, password, username)
+VALUES ('lilyartsyfartsy@himym.com', 'password', 'slapbetcommisioner');
+INSERT INTO users (email, password, username)
+VALUES ('iheartotakon@gmail.com', 'password', 'ssnake');
+INSERT INTO users (email, password, username)
+VALUES ('liquidsnake@yahoo.com', 'password', 'sunglasses');
+INSERT INTO users (email, password, username)
+VALUES ('pewpew@gmail.com', 'password', 'revolver-ocelot');
+INSERT INTO users (email, password, username)
+VALUES ('cardboard-box-a@toheliport.com', 'password', 'just-a-box');
+INSERT INTO users (email, password, username)
+VALUES ('nastasharomanenko@russia.com', 'password', 'weaponsmas');
+INSERT INTO users (email, password, username)
+VALUES ('kingdom@hearts.com', 'password', 'sora');
+INSERT INTO users (email, password, username)
+VALUES ('cloud@ff7.com', 'password', 'cloud');
+INSERT INTO users (email, password, username)
+VALUES ('ellie@tlou2.com', 'password', 'ellie');
+INSERT INTO users (email, password, username)
+VALUES ('Geralt@blackiven.com', 'password', 'Geralt');
+INSERT INTO users (email, password, username)
+VALUES ('Triss@merigold.com', 'password', 'Triss');
