8aad3e561e62f0812dbc6e738859d2ec93c9145c
Josh <josh.lerner95@gmail.com>
2020-12-01 17:46:15 +0000

    made get route, start post route

--- routes/lists.js
+++ routes/lists.js
@@ -1,33 +1,69 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
 
   router.get("/", (req, res) => {
-    res.render("list");
+    const userID = req.session["user_id"]
+     console.log(req.session, "sesh")
+    if (userID) {
+      (db.query(`SELECT items.name, items.description, categories.category, users.username
+     FROM lists
+     JOIN items ON list_id = lists.id
+     JOIN categories ON category_id = categories.id
+     JOIN users ON user_id = users.id
+     WHERE users.id = $1;
+     `, [userID]))
+        .then(data => {
+          const lists = data.rows;
+          // console.log("DATA:", lists)
+          // res.json({ lists });
+          const templateVars = { lists: lists }
+          res.render("list", templateVars)
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+    } else {
+      res.redirect('/api/credentials')
+    }
   })
 
-  router.get("/", (req, res) => {
-        db.query(`SELECT items.name, items.description, categories.category, users.username
-        FROM lists
-        JOIN items ON list_id = lists.id
-        JOIN categories ON category_id = categories.id
-        JOIN users ON user_id = users.id
-        WHERE lists.id = 2;
-        `, )
-      .then(data => {
-        const lists = data.rows;
-        // console.log("DATA:", lists)
-        // res.json({ lists });
-        const templateVars = {lists: lists}
-        res.render("list", templateVars)
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
+  // router.post("/", (req, res) => {
+
+  // })
+
+  return router;
+};
+
+
+// router.get("/", (req, res) => {
+//   db.query(`SELECT items.name, items.description, categories.category, users.username
+//         FROM lists
+//         JOIN items ON list_id = lists.id
+//         JOIN categories ON category_id = categories.id
+//         JOIN users ON user_id = users.id
+//         WHERE lists.id = 2;
+//         `, )
+//     .then(data => {
+//       const lists = data.rows;
+//       // console.log("DATA:", lists)
+//       // res.json({ lists });
+//       const templateVars = { lists: lists }
+//       res.render("list", templateVars)
+//     })
+//     .catch(err => {
+//       res
+//         .status(500)
+//         .json({ error: err.message });
+//     });
+// });
+
+// return router;
+// };
+
 
 //   router.post("/", (req,res) => {
 //    db.query(`INSERT INTO items (name, description, favourite, list_id)
@@ -40,11 +76,7 @@ module.exports = (db) => {
 
 
 // res.redirect('/api/lists')
-//   })
-  return router;
-};
-
-
+//  })
 
 
 
