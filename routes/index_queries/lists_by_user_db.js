7fe2bf385ff79dbba3fa8738db01c392fd6103bd
emtupp <emilyc.tupper@gmail.com>
2020-12-01 22:28:57 +0000

    change routes so they take in the userID cookie and use it to render nav bar

--- routes/index_queries/lists_by_user_db.js
+++ routes/index_queries/lists_by_user_db.js
@@ -5,6 +5,7 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     const userID = req.session["user_id"];
     // const user = users[req.session.user_id]
+    if (userID) {
     db.query(`
     SELECT lists.title, lists.id, items.name
     FROM lists
@@ -14,13 +15,30 @@ module.exports = (db) => {
     `, [userID])
     .then(data => {
       const list = data.rows;
-        res.json({ list });
+      res.json({ list });
       })
       .catch(err => {
         res
           .status(500)
           .json({ error: err.message });
       });
+    } else {
+      (db.query(`
+      SELECT lists.title, lists.id, items.name
+      FROM lists
+      JOIN items ON list_id = lists.id
+      WHERE lists.public = true;
+     `))
+        .then(data => {
+        const list = data.rows;
+        res.json({ list });
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+      }
   });
   return router;
 };
