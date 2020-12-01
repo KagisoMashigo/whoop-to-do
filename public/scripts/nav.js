7463a53960080ada78fc89d24c6079936f7d9bbc
emtupp <emilyc.tupper@gmail.com>
2020-12-01 23:47:06 +0000

    create js file for nav logic

---
+++ public/scripts/nav.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+  $("#food").on("click", function() {
+    console.log( "clicked food" );
+  });
+
+  $("#movies").on("click", function() {
+    console.log( "clicked movies" );
+  });
+
+  $("#books").on("click", function() {
+    console.log( "clicked books" );
+  });
+
+  $("#products").on("click", function() {
+    console.log( "clicked products" );
+  });
+
+  $("#new").on("click", function() {
+    console.log( "clicked new" );
+  });
+
+})
