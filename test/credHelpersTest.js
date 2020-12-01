87f95eefb9a3100a746e924c5784f5b8ad045104
Iso <kagiso.k.mashigo@gmial.com>
2020-12-01 19:32:36 +0000

    add intial test file

---
+++ test/credHelpersTest.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+const { getUserByEmail } = require('../credHelpersTest.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail(testUsers, "user@example.com")
+    const expectedOutput = testUsers["userRandomID"];
+    assert.strictEqual(user, expectedOutput, 'these users strictly match');
+  });
+
+  it('should return undefined if no match', function() {
+    const user = getUserByEmail(testUsers, "noUser@example.com")
+    const expectedOutput = undefined;
+    assert.strictEqual(user, expectedOutput, 'these users strictly match');
+  });
+});
