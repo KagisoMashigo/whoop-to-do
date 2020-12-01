349e72db8b3ad4ebf200924495845f6c00fd60d1
Josh <josh.lerner95@gmail.com>
2020-12-01 02:32:37 +0000

    work on get and posts

--- routes/lists.js
+++ routes/lists.js
@@ -2,13 +2,25 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+
   router.get("/", (req, res) => {
-    // console.log('trying to get lists', db)
-    db.query(`SELECT * FROM lists;`)
+    res.render("list");
+  })
+
+  router.get("/", (req, res) => {
+        db.query(`SELECT items.name, items.description, categories.category, users.username
+        FROM lists
+        JOIN items ON list_id = lists.id
+        JOIN categories ON category_id = categories.id
+        JOIN users ON user_id = users.id
+        WHERE lists.id = 2;
+        `, )
       .then(data => {
         const lists = data.rows;
         // console.log("DATA:", lists)
-        res.json({ lists });
+        // res.json({ lists });
+        const templateVars = {lists: lists}
+        res.render("list", templateVars)
       })
       .catch(err => {
         res
@@ -16,5 +28,24 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+//   router.post("/", (req,res) => {
+//    db.query(`INSERT INTO items (name, description, favourite, list_id)
+//    VALUES
+//    ('$1', '$2', false, $3);
+//    `, [name, description, listid])
+
+
+
+
+
+// res.redirect('/api/lists')
+//   })
   return router;
 };
+
+
+
+
+
+
