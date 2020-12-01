5d5cfc7a467d3856fb266d56bc73f7f0e43ee560
emtupp <emilyc.tupper@gmail.com>
2020-12-01 01:48:17 +0000

    add helper function to be used in the render of the page

--- routes/index_queries/lists_by_user.js
+++ routes/index_queries/lists_by_user.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router  = express.Router();
 
+const userListDB = function(id, db) {
+  let userDB = {};
+  for (let item in db) {
+    if (id === db[item].userID) {
+      userDB[item] = db[item];
+    }
+  }
+  return userDB;
+};
+
 module.exports = (db, user) => {
   router.get("/", (req, res) => {
+    // const user = users[req.session.user_id]
     db.query(`
-    SELECT lists.title, items.name
+    SELECT lists.title, lists.id, items.name
     FROM lists
     JOIN items ON list_id = lists.id
     JOIN users ON user_id = users.id
@@ -14,7 +25,6 @@ module.exports = (db, user) => {
       const list = data.rows;
       const templateVars = { list: list }
       res.render("index", templateVars);
-      // res.render("index", templateVars);
       })
       .catch(err => {
         res
