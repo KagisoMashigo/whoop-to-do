eca9f6e962314b8f1836bd5b9bec0865a1655e29
Iso <kagiso.k.mashigo@gmial.com>
2020-12-02 16:36:15 +0000

    lists commented out waiting on fucntionality

--- routes/lists.js
+++ routes/lists.js
@@ -1,56 +1,55 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-module.exports = (db) => {
+// module.exports = (db) => {
 
-  router.get("/", (req, res) => {
-    const userID = req.session["user_id"]
-     console.log(req.session, "sesh")
-    if (userID) {
-      (db.query(`SELECT items.name, items.description, categories.category, users.username
-     FROM lists
-     JOIN items ON list_id = lists.id
-     JOIN categories ON category_id = categories.id
-     JOIN users ON user_id = users.id
-     WHERE users.id = $1;
-     `, [userID]))
-        .then(data => {
-          const lists = data.rows;
-          // console.log("DATA:", lists)
-          // res.json({ lists });
-          const templateVars = { lists, userID }
-          res.render("list", templateVars)
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
-    } else {
-      res.redirect("/api/credentials")
-    }
-  })
+//   router.get("/", (req, res) => {
+//     const userID = req.session["user_id"]
+//      console.log("ID: ", userID)
+//     if (userID) {
+//       (db.query(`SELECT items.name, items.description, categories.category, users.username
+//      FROM lists
+//      JOIN items ON list_id = lists.id
+//      JOIN categories ON category_id = categories.id
+//      JOIN users ON user_id = users.id
+//      WHERE users.id = $1;
+//      `, [userID]))
+//         .then(data => {
+//           const lists = data.rows;
+//           console.log(lists)
+//           const templateVars = { lists, userID }
+//           res.render("list", templateVars)
+//         })
+//         .catch(err => {
+//           res
+//             .status(500)
+//             .json({ error: err.message });
+//         });
+//     } else {
+//       res.redirect("/api/credentials")
+//     }
+//   })
 
-  router.post("/", (req, res) => {
-    const userID = req.session["user_id"];
-    const text = req.body.text;
-    console.log(req.body.text)
-    db.query(`UPDATE items
-    SET list_id = $1
-    WHERE name LIKE '% $2 %';
-    `, [ 7, text])
-    .then(item => {
-      res.redirect("/api/lists")
-    })
-  })
+//   router.post("/", (req, res) => {
+//     const userID = req.session["user_id"];
+//     const text = req.body.text;
+//     console.log(req.body.text)
+//     db.query(`UPDATE items
+//     SET list_id = $1
+//     WHERE name LIKE '% $2 %';
+//     `, [ 7, text])
+//     .then(item => {
+//       res.redirect("/api/lists")
+//     })
+//   })
 
   // router.post("/", (req, res) => {
   //   req.session["user_id"] = null;
   //   res.redirect("/api/credentials");
   // });
 
-  return router;
-};
+//   return router;
+// };
 
 
 // router.get("/", (req, res) => {
