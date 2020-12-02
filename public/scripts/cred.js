5a44f8be6eba04f80fc6aa986be80b8192f683f2
Iso <kagiso.k.mashigo@gmial.com>
2020-12-02 22:57:09 +0000

    work on error section

--- public/scripts/cred.js
+++ public/scripts/cred.js
@@ -24,21 +24,21 @@ $(document).ready(() => {
   // Error handling
   
   // test for login form
-  $("#signup").on('click', function() {
-    console.log("Clicked!")
-    $(".message").css("transform", "translateX(100%)");
-    if ($(".message").hasClass("login")) {
-      $(".message").removeClass("login");
-    }
-    $(".message").addClass("signup");
-  });
+$("#signup").on('click', function() {
+  console.log("Clicked!")
+  $(".message").css("transform", "translateX(100%)");
+  if ($(".message").hasClass("login")) {
+    $(".message").removeClass("login");
+  }
+  $(".message").addClass("signup");
+});
 
-  $("#login").on('click', function() {
-    console.log("Clicked!")
-    $(".message").css("transform", "translateX(0)");
-    if ($(".message").hasClass("login")) {
-      $(".message").removeClass("signup");
-    }
-    $(".message").addClass("login");
-  });
+$("#login").on('click', function() {
+  console.log("Clicked!")
+  $(".message").css("transform", "translateX(0)");
+  if ($(".message").hasClass("login")) {
+    $(".message").removeClass("signup");
+  }
+  $(".message").addClass("login");
+});
 });
