4148f6f1b178437f5b2e06d184540d18edca0406
Josh <josh.lerner95@gmail.com>
2020-12-02 20:57:10 +0000

    made functionality of edit and delete button

--- public/scripts/helpersLists.js
+++ public/scripts/helpersLists.js
@@ -1,7 +1,14 @@
+// Fixes HTML vulnerabilities
+const escape = function (str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 // This is responsible for toggling the form using the arrow on the page
 const toggleForm = () => {
   const $addItem = $('.add_item');
-  $addItem.on('click', function() {
+  $addItem.on('click', function () {
     const $itemForm = $('.item_toggle');
     if ($itemForm.is(":visible")) {
       $itemForm.slideUp(750);
@@ -16,9 +23,113 @@ $(document).ready(() => {
   toggleForm();
   // test for login form
 
+  $('.display-personal-lists').on('click', '#delete', (event) => {
+    // console.log(event.currentTarget)
+    const itemId = $(event.currentTarget).parent().parent().parent().attr("id")
+    //  console.log("itemID:", itemId)
+    $.ajax(`/lists/delete/${itemId}`, {method: "POST"})
+    .then((response) => {
+    getList()
+console.log(response)
+    })
+  })
 
+  $('.display-personal-lists').on('click', '#fave', (event) => {
+    // console.log(event.currentTarget)
+    const itemId = $(event.currentTarget).parent().parent().parent().attr("id")
+    //  console.log("itemID:", itemId)
+    $.ajax(`/lists/fave/${itemId}`, {method: "POST"})
+    .then((response) => {
+    getList()
+console.log(response)
+    })
+  })
+
+
+})
+
+
+
+const addItems = function ($list, items) {
+  // console.log("add item's item is: ", item)
+  $('container', $list).append(`
+   <div class ="name">
+   ${escape(items.name[0])}
+  </div>
+  <div class="description">
+  ${escape(items.description[0])}
+  </div>`
+  );
+};
+
+
+const createNewPersonalList = function (items) {
+  let $list = $(`
+    <li id="${items[0]}">
+      <label for="todo-${items[0]}">
+        <input id="todo-${items[0]}" type="checkbox">
+          <container class ="container">
+          </container>
+      </label>
+   <footer class= "buttons">
+       <form class="item_delete">
+       <button id ="delete" type="submit">
+         <span class="glyphicon glyphicon-trash"></span>
+       </button>
+     </form>
+     <form class="item_fave">
+       <button id="fave" type="submit">
+         <span class="glyphicon glyphicon-heart"></span>
+         </button>
+       </form>
+       </footer>
+    </li>`);
+  addItems($list, items[1]);
+  return $list
+
+
+  // edit button
+  // <button id="edit">
+  //        <span class="glyphicon glyphicon-edit"></span>
+  //      </button>
+
+
+
+};
+
+// Renders the lists
+const renderPersonalLists = function (lists) {
+  let dbObj = {};
+  for (let list in lists) {
+    for (let item of lists[list]) {
+      if (!dbObj[item.id]) {
+        dbObj[item.id] = { name: [item.name], description: [item.description] }
+        // dbObj[item.title] = [item.name];
+      } else {
+        dbObj[item.id].name.push(item.name);
+        dbObj[item.id].description.push(item.description);
+      }
+    }
+  }
+  for (let list of Object.entries(dbObj)) {
+    console.log(list)
+    const $list = createNewPersonalList(list);
+    $('#display-personal-lists').append($list);
+  }
+};
+
+
+
+
+$(document).ready(function () {
+getList();
+  console.log("Ready to go!");
 });
 
-
-
-
+  const getList = () => {
+    $.ajax('/api/lists', { method: 'GET' })
+    .then((lists) => {
+      console.log('ajax promise argument: ', lists)
+      renderPersonalLists(lists)
+    });
+  }
