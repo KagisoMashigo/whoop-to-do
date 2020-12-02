60d110ef5f20c46c9a674315354a2e528d9126da
Iso <kagiso.k.mashigo@gmial.com>
2020-12-02 16:32:30 +0000

    add hashedpasswords to seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,31 +1,31 @@
 -- Users table seeds here (Example)
 INSERT INTO users (email, password, username)
-VALUES ('tedmosbyarchitect@himym.com', 'password', 'theodore');
+VALUES ('tedmosbyarchitect@himym.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'theodore');
 INSERT INTO users (email, password, username)
-VALUES ('legen_dary@himym.com', 'password', 'barnacle');
+VALUES ('legen_dary@himym.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'barnacle');
 INSERT INTO users (email, password, username)
-VALUES ('judgefudge@himym.com', 'password', 'marshmellow');
+VALUES ('judgefudge@himym.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'marshmellow');
 INSERT INTO users (email, password, username)
-VALUES ('robinmetronews1@himym.com', 'password', 'hoser');
+VALUES ('robinmetronews1@himym.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'hoser');
 INSERT INTO users (email, password, username)
-VALUES ('lilyartsyfartsy@himym.com', 'password', 'slapbetcommisioner');
+VALUES ('lilyartsyfartsy@himym.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'slapbetcommisioner');
 INSERT INTO users (email, password, username)
-VALUES ('iheartotakon@gmail.com', 'password', 'ssnake');
+VALUES ('iheartotakon@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'ssnake');
 INSERT INTO users (email, password, username)
-VALUES ('liquidsnake@yahoo.com', 'password', 'sunglasses');
+VALUES ('liquidsnake@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'sunglasses');
 INSERT INTO users (email, password, username)
-VALUES ('pewpew@gmail.com', 'password', 'revolver-ocelot');
+VALUES ('pewpew@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'revolver-ocelot');
 INSERT INTO users (email, password, username)
-VALUES ('cardboard-box-a@toheliport.com', 'password', 'just-a-box');
+VALUES ('cardboard-box-a@toheliport.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'just-a-box');
 INSERT INTO users (email, password, username)
-VALUES ('nastasharomanenko@russia.com', 'password', 'weaponsmas');
+VALUES ('nastasharomanenko@russia.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'weaponsmas');
 INSERT INTO users (email, password, username)
-VALUES ('kingdom@hearts.com', 'password', 'sora');
+VALUES ('kingdom@hearts.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'sora');
 INSERT INTO users (email, password, username)
-VALUES ('cloud@ff7.com', 'password', 'cloud');
+VALUES ('cloud@ff7.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'cloud');
 INSERT INTO users (email, password, username)
-VALUES ('ellie@tlou2.com', 'password', 'ellie');
+VALUES ('ellie@tlou2.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'ellie');
 INSERT INTO users (email, password, username)
-VALUES ('Geralt@blackiven.com', 'password', 'Geralt');
+VALUES ('Geralt@blackiven.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'Geralt');
 INSERT INTO users (email, password, username)
-VALUES ('Triss@merigold.com', 'password', 'Triss');
+VALUES ('Triss@merigold.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'Triss');
