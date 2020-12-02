57e4836d5b4a843f45f38cbe88b600cbb3b2fe22
Iso <kagiso.k.mashigo@gmial.com>
2020-12-02 22:11:18 +0000

    remove comments

--- routes/lists.js
+++ routes/lists.js
@@ -40,7 +40,7 @@ module.exports = (db) => {
     console.log(req.body.text)
     db.query(`UPDATE items
     SET list_id =$1
-    WHERE name ILIKE '%'||$2||'%';
+    WHERE name LIKE '%'||$2||'%';
     `, [listID, text])
     .then(item => {
       res.send(200)
@@ -87,55 +87,5 @@ module.exports = (db) => {
 
 
   })
-  // router.post("/", (req, res) => {
-  //   req.session["user_id"] = null;
-  //   res.redirect("/api/credentials");
-  // });
-
   return router;
 };
-
-
-
-// router.get("/", (req, res) => {
-//   db.query(`SELECT items.name, items.description, categories.category, users.username
-//         FROM lists
-//         JOIN items ON list_id = lists.id
-//         JOIN categories ON category_id = categories.id
-//         JOIN users ON user_id = users.id
-//         WHERE lists.id = 2;
-//         `, )
-//     .then(data => {
-//       const lists = data.rows;
-//       // console.log("DATA:", lists)
-//       // res.json({ lists });
-//       const templateVars = { lists: lists }
-//       res.render("list", templateVars)
-//     })
-//     .catch(err => {
-//       res
-//         .status(500)
-//         .json({ error: err.message });
-//     });
-// });
-
-// return router;
-// };
-
-
-//   router.post("/", (req,res) => {
-//    db.query(`INSERT INTO items (name, description, favourite, list_id)
-//    VALUES
-//    ('$1', '$2', false, $3);
-//    `, [name, description, listid])
-
-
-
-
-
-// res.redirect('/api/lists')
-//  })
-
-
-
-
