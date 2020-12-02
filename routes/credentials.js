caf05c238daafc0ef89c48508c58439f3a576835
Iso <kagiso.k.mashigo@gmial.com>
2020-12-02 16:56:37 +0000

    clean files from merge

--- routes/credentials.js
+++ routes/credentials.js
@@ -1,11 +1,8 @@
 /*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
- // POA - 1: logout, css for page, refractoring & testing
+// POA - 1: logout, css for page, refractoring (halfway done here, need to make modular now) & testing
 //  Yesterday: was making logout on lists page
 const bcrypt = require('bcrypt');
 const express = require('express');
