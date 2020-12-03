ca99522e2ee0367916ea53d5db3f7cd098929d14
Iso <kagiso.k.mashigo@gmial.com>
2020-12-03 04:09:34 +0000

    modularize code and start list maker

--- routes/credHelpers.js
+++ routes/credHelpers.js
@@ -6,7 +6,7 @@
     WHERE email = $1;
     `;
     const values = [email];
-    return db.query(sqlQuery, values)
+    return db.query(sqlQuery, values);
   };
 
   const addUser = (db, email, hashedPassword, username) => {
@@ -14,8 +14,8 @@
     INSERT INTO users (email, password, username)
     VALUES ($1, $2, $3);
     `;
-    const values = [email, hashedPassword, username]
-    return db.query(sqlQuery, values)
+    const values = [email, hashedPassword, username];
+    return db.query(sqlQuery, values);
   }
 
   const displayPublicLists = (db) => {
@@ -25,7 +25,19 @@
     JOIN items ON list_id = lists.id
     WHERE lists.public = true;
     `;
-    return db.query(sqlQuery)
+    return db.query(sqlQuery);
   }
+
+  // const defaultListMaker = () => {
+  //   INSERT INTO lists (title, public, category_id, user_id)
+  //   VALUES ('Movies', true, 1, 2);
+  //   INSERT INTO lists (title, public, category_id, user_id)
+  //   VALUES ('Books', true, 4, 2);
+  //   INSERT INTO lists (title, public, category_id, user_id)
+  //   VALUES ('Restaurants', true, 2, 7);
+  //   INSERT INTO lists (title, public, category_id, user_id)
+  //   VALUES ('Products', false, 3, 8);
+  //   INSERT INTO lists (title, public, category_id, user_id)
+  // }
 
 module.exports = { getUserByEmail, displayPublicLists, addUser };
