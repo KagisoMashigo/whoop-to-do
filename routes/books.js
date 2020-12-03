356cf3e1dc598c8e4c3a4ae0374b9c9032dcc2f7
Iso <kagiso.k.mashigo@gmial.com>
2020-12-03 21:03:17 +0000

    books route added

--- routes/books.js
+++ routes/books.js
@@ -1,38 +1,38 @@
-// const express = require('express');
-// const router  = express.Router();
-// const axios = require('axios');
+const express = require('express');
+const router  = express.Router();
+const axios = require('axios');
 
 
-// module.exports = (db) => {
+module.exports = (db) => {
 
-//   router.post("/", (req, res) => {
+  router.post("/", (req, res) => {
 
-//     const { title, author} = req.body;
+    const { title, author} = req.body;
 
-//     axios.get('https://www.googleapis.com/books/v1/volumes?q=harry+potter+inauthor:rowling&key=AIzaSyBT-iVATn5jscoq7CUm_qUmzb_s9NdR01E')
-//     .then(function (response) {
-//       // handle success
-//       console.log(response);
-//       db.query(
-//         `INSERT INTO books (title, descritption, author)
-//         VALUES ($1, $2, $3);
-//         `
-//         )
-//         .then(dbres => {
-//           // last step if all goes well
-//           res.redirect("list")
-//         })
-//     })
-//     .catch(function (error) {
-//       // handle error
-//       console.log(error);
-//     })
+    axios.get('https://www.googleapis.com/books/v1/volumes?q=harry+potter+inauthor:rowling&key=AIzaSyBT-iVATn5jscoq7CUm_qUmzb_s9NdR01E')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+      db.query(
+        `INSERT INTO books (title, descritption, author)
+        VALUES ($1, $2, $3);
+        `
+        )
+        .then(dbres => {
+          // last step if all goes well
+          res.redirect("list")
+        })
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
 
-//   });
+  });
 
-//   return router;
-// };
+  return router;
+};
 
-// const fetchMyIP = function(callback) {
+const fetchMyIP = function(callback) {
   
-// };
+};
