44321c27bfbbfc58a8e6321c0781208424bd0209
Josh <josh.lerner95@gmail.com>
2020-12-04 04:44:02 +0000

    add data to db

--- db/seeds/03_lists.sql
+++ db/seeds/03_lists.sql
@@ -4,8 +4,8 @@ VALUES ('Movies', true, 1, 2);
 INSERT INTO lists (title, public, category_id, user_id)
 VALUES ('Books', true, 4, 2);
 INSERT INTO lists (title, public, category_id, user_id)
-VALUES ('Restaurants', true, 2, 7);
+VALUES ('Restaurants', true, 2, 2);
 INSERT INTO lists (title, public, category_id, user_id)
-VALUES ('Products', false, 3, 8);
+VALUES ('Products', false, 3, 2);
 INSERT INTO lists (title, public, category_id, user_id)
 VALUES ('Miscellaneous', false, 5, 14);
