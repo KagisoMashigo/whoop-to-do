547628df7fa8470e9bc354728bc14cf9c018f68d
emtupp <emilyc.tupper@gmail.com>
2020-12-04 02:29:33 +0000

    add api request for movies

--- routes/api_requests/TMDb.js
+++ routes/api_requests/TMDb.js
@@ -2,20 +2,24 @@ const request = require('request');
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (formInput) => {
-  console.log('Trying TMDB function');
-  router.get("/", (req, res) => {
-    request(`https://api.themoviedb.org/3/search/movie?api_key=0ee58801eee865ef1538b59630b19b32&query=${formInput}`, (error, response, body) => {
-      const data = JSON.parse(body);
-      const movies = {};
-      for (let entries of data.results) {
-        let title = entries.title;
-        let overview = entries.overview;
-        movies[title] = overview;
-      }
-      console.log(Object.keys(movies).length)
-      res.json({ movies });
-    })
-  });
-  return router;
+
+const fetchMovie = (req, res) => {
+  request(`https://api.themoviedb.org/3/search/movie?api_key=0ee58801eee865ef1538b59630b19b32&query=${req.body.search}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    const movies = [];
+    for (let entries of data.results) {
+      let title = entries.title;
+      let overview = entries.overview;
+      movies.push({ title, overview });
+    }
+    res.json({ movies });
+  })
 };
+
+
+module.exports = (db) => {
+
+  router.post("/", fetchMovie)
+  return router;
+
+}
